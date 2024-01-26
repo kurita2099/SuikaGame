cc57b9a17c4c6541e2fb47a06fbed675e65c75b7
kurita2099 <takuri00+github@gmail.com>
2024-01-26 15:31:58 +0900

    同じ果物が触れるとイベント発生

--- js/matterjs/advanced shape creation.js
+++ js/matterjs/advanced shape creation.js
@@ -1,3 +1,58 @@
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+        //  Load sprite sheet generated with TexturePacker
+        this.load.atlas('sheet', 'assets/physics/fruit-sprites.png', 'assets/physics/fruit-sprites.json');
+
+        //  Load body shapes from JSON file generated using PhysicsEditor
+        this.load.json('shapes', 'assets/physics/fruit-shapes.json');
+    }
+
+    create ()
+    {
+        this.matter.world.setBounds(0, 0, 600, 800);
+
+        const shapes = this.cache.json.get('shapes');
+
+        this.add.image(0, 0, 'sheet', 'background').setOrigin(0, 0);
+
+        const ground = this.matter.add.sprite(0, 0, 'sheet', 'ground', { shape: shapes.ground });
+
+        this.matter.alignBody(ground, 300, 800, Phaser.Display.Align.BOTTOM_CENTER);
+
+        this.matter.add.sprite(200, 50, 'sheet', 'crate', { shape: shapes.crate });
+        this.matter.add.sprite(250, 250, 'sheet', 'orange', { shape: shapes.banana });
+        this.matter.add.sprite(360, 50, 'sheet', 'banana', { shape: shapes.orange });
+        this.matter.add.sprite(400, 250, 'sheet', 'banana', { shape: shapes.cherries });
+
+        const shapeKeys = [ 'crate', 'banana', 'orange', 'cherries' ];
+        let fruitname ='orange';
+        const fruit_ = this.add.sprite(0, 20, 'sheet', fruitname).setScale(0.3);
+        this.input.on('pointerdown', function (pointer)
+        {
+            
+
+            this.matter.add.image(fruit_.x, fruit_.y+50, 'sheet', fruitname, { shape: shapes[fruitname] });
+            fruitname = Phaser.Utils.Array.GetRandom(shapeKeys);
+            fruit_.setTexture('sheet', fruitname);
+
+        }, this);
+        this.input.on('pointermove', function (pointer)
+        {
+            fruit_.x = pointer.x;
+        }, this);
+        this.matter.world.on('collisionstart', function (event) {
+              event.pairs.forEach(function(item){
+                    if(item.bodyA.label == item.bodyB.label){
+                        alert(item.bodyA.label);
+                    }                 
+              });
+        },this)
+
+    }
+}
+
 /*
  *  This example show how to load complex shapes created with PhysicsEditor (https://www.codeandweb.com/physicseditor)
  */
@@ -7,10 +62,7 @@ const config = {
     parent: 'phaser-example',
     width: 600,
     height: 800,
-    scene: {
-        preload: preload,
-        create: create
-    },
+    scene: Example,
     physics: {
         default: 'matter',
         matter: {
@@ -19,41 +71,4 @@ const config = {
     }
 };
 
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-    //  Load sprite sheet generated with TexturePacker
-    this.load.atlas('sheet', 'assets/physics/fruit-sprites.png', 'assets/physics/fruit-sprites.json');
-
-    //  Load body shapes from JSON file generated using PhysicsEditor
-    this.load.json('shapes', 'assets/physics/fruit-shapes.json');
-}
-
-function create ()
-{
-    this.matter.world.setBounds(0, 0, 600, 800);
-
-    var shapes = this.cache.json.get('shapes');
-
-    this.add.image(0, 0, 'sheet', 'background').setOrigin(0, 0);
-
-    var ground = this.matter.add.sprite(0, 0, 'sheet', 'ground', { shape: shapes.ground });
-
-    this.matter.alignBody(ground, 300, 800, Phaser.Display.Align.BOTTOM_CENTER);
-
-    this.matter.add.sprite(200, 50, 'sheet', 'crate', { shape: shapes.crate });
-    this.matter.add.sprite(250, 250, 'sheet', 'banana', { shape: shapes.banana });
-    this.matter.add.sprite(360, 50, 'sheet', 'orange', { shape: shapes.orange });
-    this.matter.add.sprite(400, 250, 'sheet', 'cherries', { shape: shapes.cherries });
-
-    var shapeKeys = [ 'crate', 'banana', 'orange', 'cherries' ];
-
-    this.input.on('pointerdown', function (pointer)
-    {
-        var fruit = Phaser.Utils.Array.GetRandom(shapeKeys);
-
-        this.matter.add.image(pointer.x, pointer.y, 'sheet', fruit, { shape: shapes[fruit] });
-
-    }, this);
-}
+const game = new Phaser.Game(config);
